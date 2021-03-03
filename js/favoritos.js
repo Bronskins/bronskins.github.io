3f97882c168cb031c4f15b6c622a9d179fb640d1
Bronskins <47313695+Bronskins@users.noreply.github.com>
2021-03-03 11:47:33 -0300

    Add files via upload

--- js/favoritos.js
+++ js/favoritos.js
@@ -18,6 +18,7 @@ let h1Border = document.getElementById('h1-border')
 let midCtn = document.getElementById('midCtn')
 let verMas = document.getElementById('verMas')
 let noResults = document.getElementById('noResults')
+let gifMax = document.getElementById('ampliarGif')
 let inputContainer = document.getElementById('input-container')
 let iconClose = document.getElementById('iconClose')
 let favs = document.getElementsByClassName('favorito')
@@ -33,8 +34,9 @@ let aux4 = 0;
 
 // Agregar al DOM el GIF
 
-function AddtoDOM(data, container, additionalClass){
+// Agregar al DOM el GIF
 
+function AddtoDOM(data, container, additionalClass){
     newDiv = document.createElement('div')
     newDiv.className = "imagen-container"
     newImg = document.createElement('img')
@@ -58,15 +60,14 @@ function AddtoDOM(data, container, additionalClass){
     fav = document.createElement('img')
     fav.className = "favorito"
     fav.id = `favorito${aux4}`
-    fav.style.width = "32px"
-    fav.style.height = "32px"
     if (localStorage.getItem(`ID${data.id}`) === null) {
         fav.setAttribute ("src", "../assets/icon-fav.svg")
 
     } else {
         fav.setAttribute ("src", "../assets/icon-fav-active.svg")
+        fav.style.width = "32px"
+        fav.style.height = "32px"
     }
-    favIcons.push(fav)
     newImg.setAttribute('src', "https://media3.giphy.com/media/" + data.id + "/giphy.gif")
     download = document.createElement('img')
     download.className = "descarga"
@@ -103,7 +104,8 @@ function AddtoDOM(data, container, additionalClass){
         location.href = data.embed_url
     })
     maximize.addEventListener('click', () => {
-        location.href = data.embed_url
+        maxGif(data.id, data.user, data.title, aux4)
+
     });
 
     let favy = document.getElementById(`favorito${aux4}`)
@@ -120,6 +122,122 @@ function AddtoDOM(data, container, additionalClass){
     })
     
     aux4++;
+}
+
+// Agrandar Gif 
+
+function maxGif(gifId, gifUser, gifTitle, aux4){
+
+fav = document.createElement('img')
+fav.className = "favorito"
+fav.id = `favorito${aux4}`
+if (localStorage.getItem(`ID${gifId}`) === null) {
+    fav.setAttribute ("src", "../assets/icon-fav.svg")
+
+} else {
+    fav.setAttribute ("src", "../assets/icon-fav-active.svg")
+    fav.style.width = "32px"
+    fav.style.height = "32px"
+}
+closeMax = document.createElement('img')
+closeMax.setAttribute('src', "../assets/close.svg")
+closeMax.style.marginBottom = "20px"
+closeMax.style.marginLeft = "675px"
+closeMax.style.cursor = "pointer"
+newDownload = document.createElement('img')
+newDownload.className = "descarga"
+newDownload.setAttribute("src", "../assets/icon-download.svg")
+newDownload.style.border = "1px solid #9CAFC3"
+newDownload.style.borderRadius = "6px"
+newDownload.style.marginLeft = "15px"
+newDownload.style.cursor = "pointer"
+let featuresDiv = document.createElement('div')
+let maxDiv = document.createElement('div')
+let titlesDiv = document.createElement('div')
+maxDiv.style.display = "flex"
+maxDiv.style.width = "695px"
+maxDiv.style.flexFlow = "row-reverse"
+maxDiv.style.justifyContent = "space-between"
+gifMax.style.display = "flex"
+gifMax.style.alignItems = "center"
+gifMax.style.justifyContent = "center"
+gifMax.style.height = "100%"
+gifMax.style.width = "100%"
+gifMax.style.position = "fixed"
+gifMax.style.top = "1px"
+gifMax.style.zIndex = "2"
+gifMax.style.flexDirection = "column"
+gifMax.style.backgroundColor = "white"
+let maxImg = document.createElement('img')
+maxImg.style.width = "695px"
+maxImg.style.height = "385px"
+maxImg.setAttribute('src', "https://media3.giphy.com/media/" + gifId + "/giphy.gif")
+maxImg.style.marginBottom = "10px"
+let userTitle = document.createElement('h3')
+userTitle.style.fontSize = "15px"
+userTitle.style.color = "black"
+userTitle.style.width = "31px"
+userTitle.style.height = "18px"
+userTitle.style.marginBottom = "10px"
+userTitle.style.textAlign = "center"
+userTitle.style.fontFamily = 'Roboto, sans-serif'
+if (gifUser === undefined){
+    userTitle.innerHTML = "User"
+    } else {
+    userTitle.innerHTML = gifUser.username
+    }
+let gifTitulo = document.createElement('h3')
+gifTitulo.style.fontSize = "16px"
+gifTitulo.style.textAlign = "center"
+gifTitulo.style.color = "black"
+gifTitulo.style.fontFamily = 'Roboto, sans-serif'
+gifTitulo.style.height = "19px"
+gifTitulo.style.zIndex = "2"
+gifTitulo.textContent = gifTitle
+fav.style.border = "1px solid #9CAFC3"
+fav.style.borderRadius = "6px"
+fav.style.cursor = "pointer"
+gifMax.appendChild(closeMax)
+gifMax.appendChild(maxImg)
+gifMax.appendChild(maxDiv)
+maxDiv.appendChild(featuresDiv)
+maxDiv.appendChild(titlesDiv)
+titlesDiv.appendChild(userTitle)
+titlesDiv.appendChild(gifTitulo)
+featuresDiv.appendChild(fav)
+featuresDiv.appendChild(newDownload)
+
+newDownload.addEventListener('click', () => {
+    fetch("https://media3.giphy.com/media/" + gifId + "/giphy.gif")
+    .then(response => response.blob().then( giphy =>{
+        saveURL = URL.createObjectURL(giphy)
+        Object.assign( link = document.createElement('a'), { 
+            href: saveURL, 
+            download: "myGif.gif"
+        }	)
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    } 	)	)
+});
+
+let favy = document.getElementById(`favorito${aux4}`)
+favy.addEventListener('click', () => {
+    if(favy.src.includes("icon-fav.svg")){
+    localStorage.setItem(`ID${gifId}`, `${gifId}`)
+    favy.setAttribute('src', "../assets/icon-fav-active.svg")
+    favy.style.width = "32px";
+    favy.style.height = "32px"
+    } else {
+        favy.setAttribute("src", "../assets/icon-fav.svg")
+        localStorage.removeItem(`ID${gifId}`, `${gifId}`)
+    }
+})
+closeMax.addEventListener('click', () => {
+gifMax.innerHTML = ""
+gifMax.style.display = "none"
+})
+
 }
 
 // Fetch FAVORITOS
